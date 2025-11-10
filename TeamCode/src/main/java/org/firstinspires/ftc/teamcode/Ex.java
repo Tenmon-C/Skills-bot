1a4188fee0ba847adb1babb18f1b3ea461342449
Blackphantom1273 <aiden.carals@gmail.com>
2025-11-10 14:50:34 -0500

    * Added comments stating what each of the steps do that way there wont be any confusion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ex.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ex.java
@@ -41,47 +41,47 @@ public class Ex extends Base {
         LeftWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         RightWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        telemetry.addData("Step 1","Driving Forward");
+        telemetry.addData("Step 1","Driving Forward");//Exits the starting zone
         telemetry.update();
         drive(33.8,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 2","Turning Right");
+        telemetry.addData("Step 2","Turning Right");//Turns to face the furthest wall
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 3","Driving Forward");
+        telemetry.addData("Step 3","Driving Forward");//Drives till it reaches the auto zone
         telemetry.update();
         drive(25,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 4","Turning Right");
+        telemetry.addData("Step 4","Turning Right");//Turns to stare at the auto zone
         telemetry.update();
         turnTo(-180,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 5","Driving Forward");
+        telemetry.addData("Step 5","Driving Forward");//Enters the auto zone
         telemetry.update();
         drive(25,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 6","Turning Right");
+        telemetry.addData("Step 6","Turning Right");//Turns to the wall that leads to the supply's
         telemetry.update();
         turnTo(-270,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 7","Driving Forward");
+        telemetry.addData("Step 7","Driving Forward");//drives into the long hallway
         telemetry.update();
         drive(18,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 8","Turning Left");
+        telemetry.addData("Step 8","Turning Left");//Turns left to be starting at the farthest wall in the hallway
         telemetry.update();
         turnTo(-180,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 7","Driving Forward");
+        telemetry.addData("Step 7","Driving Forward");//Drives to and picks up the supply closest to the safe zone
         telemetry.update();
         drive(64,0.3);
         sleep(1000);
@@ -89,12 +89,12 @@ public class Ex extends Base {
         Arm.setTargetPosition(60);
         TelemeryClear();
 
-        telemetry.addData("Step 8","Turning Left");
+        telemetry.addData("Step 8","Turning Left");//Turns to the safe zone
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 9","Drive Forward");
+        telemetry.addData("Step 9","Drive Forward");//Drives to the safe zone and drops the supply when there
         telemetry.update();
         drive(10,0.3);
         Arm.setTargetPosition(0);
@@ -102,17 +102,17 @@ public class Ex extends Base {
         ClawPos(.55);
         TelemeryClear();
 
-        telemetry.addData("Step 10","Driving Backwards");
+        telemetry.addData("Step 10","Driving Backwards");//Drives backwards out of the safe zone to where it picked up the supply
         telemetry.update();
         drive(-10, 0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 11","Turning Left");
+        telemetry.addData("Step 11","Turning Left");//Turns to face the other supply across the hallway
         telemetry.update();
         turnTo(0,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 12","Driving Forward");
+        telemetry.addData("Step 12","Driving Forward");//drives to the supply and grabs the supply
         telemetry.update();
         drive(64,0.3);
         TelemeryClear();
@@ -120,23 +120,23 @@ public class Ex extends Base {
         sleep(800);
         Arm.setTargetPosition(30);
 
-        telemetry.addData("Step 13","Turning Right");
+        telemetry.addData("Step 13","Turning Right");//Turns to the other side of the hallway
         telemetry.update();
         turnTo(-180,0.2);
         Arm.setTargetPosition(80);
         TelemeryClear();
 
-        telemetry.addData("Step 14","Driving Forward");
+        telemetry.addData("Step 14","Driving Forward");//Drives to the end of the hallway
         telemetry.update();
         drive(64,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 15","Turning Left");
+        telemetry.addData("Step 15","Turning Left");//Turns toward the safe zone
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 16", "Driving forward");
+        telemetry.addData("Step 16", "Driving forward");//Drives into the save zone and drops the supply off
         telemetry.update();
         drive(10,0.3);
         Arm.setTargetPosition(0);
@@ -144,27 +144,27 @@ public class Ex extends Base {
         ClawPos(0.55);
         TelemeryClear();
         
-        telemetry.addData("Step 17","Driving Backwords");
+        telemetry.addData("Step 17","Driving Backwords");//Leaves the safe zone
         telemetry.update();
         drive(-10,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 18","Turning Left");
+        telemetry.addData("Step 18","Turning Left");//Turns to face the other side of the hallway
         telemetry.update();
         turnTo(-0,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 19","Driving Forward");
+        telemetry.addData("Step 19","Driving Forward");//Drives to the first spot where the ECU fan could be
         telemetry.update();
         drive(17.75,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 20","Turning Right");
+        telemetry.addData("Step 20","Turning Right");//Turns to face the first location = pos 1
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 21", "Driving Forward");
+        telemetry.addData("Step 21", "Driving Forward");//Drives and picks up the ECU fan in pos 1
         telemetry.update();
         drive(7,0.3);
         TelemeryClear();
@@ -172,40 +172,40 @@ public class Ex extends Base {
         sleep(500);
         Arm.setTargetPosition(30);
 
-        telemetry.addData("Step 22", "Turning Left");
+        telemetry.addData("Step 22", "Turning Left");//Turns to face the ECU fan zone
         telemetry.update();
         turnTo(90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 23","Driving Forward");
+        telemetry.addData("Step 23","Driving Forward");//Drives to the ECU fan zone and drops the fan off
         telemetry.update();
         Arm.setTargetPosition(100);
         drive(17,0.3);
         TelemeryClear();
         ClawPos(0.55);
 
-        telemetry.addData("Step 24","Driving Backwords");
+        telemetry.addData("Step 24","Driving Backwords");//drives backwards to not nock off the ECU fan
         telemetry.update();
         drive(-10,0.3);
         Arm.setTargetPosition(0);
         TelemeryClear();
 
-        telemetry.addData("Step 25","Turning Right");
+        telemetry.addData("Step 25","Turning Right");//Turns to face the other side of the hallway again
         telemetry.update();
         turnTo(0,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 26","Drving Forward");
+        telemetry.addData("Step 26","Driving Forward");//Drives to the second ECU fan location = pos 2
         telemetry.update();
         drive(6,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 27","Turning Right");
+        telemetry.addData("Step 27","Turning Right");//Turns to look at pos 2
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 28","Driving Forward");
+        telemetry.addData("Step 28","Driving Forward");//Drives to pos 2 and grabs it
         telemetry.update();
         drive(7,0.3);
         ClawPos(0.4);
@@ -213,39 +213,39 @@ public class Ex extends Base {
         Arm.setTargetPosition(100);
         TelemeryClear();
 
-        telemetry.addData("Step 29","Turning Left");
+        telemetry.addData("Step 29","Turning Left");//Turns around to look at the ECU fan zone
         telemetry.update();
         turnTo(90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 29","Driving Forward");
+        telemetry.addData("Step 29","Driving Forward");//Drives and drops the ECU fan off
         telemetry.update();
         drive(17,0.3);
         TelemeryClear();
         ClawPos(0.55);
 
-        telemetry.addData("Step 30","Drivng Backwards");
+        telemetry.addData("Step 30","Driving Backwards");//Backs up away from the ECU fan zone
         telemetry.update();
         drive(-10,0.3);
         Arm.setTargetPosition(0);
         TelemeryClear();
 
-        telemetry.addData("Step 31","Turning Right");
+        telemetry.addData("Step 31","Turning Right");//Turns to face the end of the hallway again
         telemetry.update();
         turnTo(0,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 32","Driving Forward");
+        telemetry.addData("Step 32","Driving Forward");//Drives to the third spot the ECU fan could be = pos 3
         telemetry.update();
         drive(6,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 33","Turning Right");
+        telemetry.addData("Step 33","Turning Right");//Turns to stare at pos 3
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 34","Driving Forward");
+        telemetry.addData("Step 34","Driving Forward");//Drives and grabs pos 3
         telemetry.update();
         drive(7,0.3);
         ClawPos(0.4);
@@ -253,64 +253,68 @@ public class Ex extends Base {
         Arm.setTargetPosition(100);
         TelemeryClear();
 
-        telemetry.addData("Step 35","Driving Backwards");
+        telemetry.addData("Step 35","Driving Backwards");//Drives backwards to old pos
         telemetry.update();
         drive(-10,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 36","Turning Right");
+        telemetry.addData("Step 36","Turning Right");//Turns to go to line up in pos 2 turn point
         telemetry.update();
         turnTo(-180,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 37","Driving Forwards");
+        telemetry.addData("Step 37","Driving Forwards");//Drives to old pos 2 spot
         telemetry.update();
         drive(6,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 38","Turning Right");
+        telemetry.addData("Step 38","Turning Right");//Turns to stare at the ECU fan zone
         telemetry.update();
         turnTo(-270,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 39","Driving Forwards");
+        telemetry.addData("Step 39","Driving Forwards");//Drives to the ECU fan zone and drops the ECU fan off
         telemetry.update();
         drive(10,0.3);
         TelemeryClear();
         ClawPos(0.55);
 
-        telemetry.addData("Step 40","Driving Backwards");
+        telemetry.addData("Step 40","Driving Backwards");//Drives backwards to old spot
         telemetry.update();
         drive(10,0.3);
         TelemeryClear();
         Arm.setTargetPosition(0);
 
-        telemetry.addData("Step 41","Turning Right");
+        telemetry.addData("Step 41","Turning Right");// turns to face the other hallway wall
         telemetry.update();
         turnTo(-0,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 42","Driving Forwards");
+        telemetry.addData("Step 42","Driving Forwards");//drives until the auto entry zone
         telemetry.update();
         drive(34.5,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 43","Turning Right");
+        telemetry.addData("Step 43","Turning Right");//Turns to stare at the auto entry zone
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 44","Driving Forwards");
+        telemetry.addData("Step 44","Driving Forwards");//Drives into the entry zone
         telemetry.update();
         drive(17,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 45","Turning Left");
+        telemetry.addData("Step 45","Turning Left");//Turns to look outside of the auto zone
         telemetry.update();
         turnTo(0,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 46", "Signaling Ready For Pass-off");
+        /*
+        From here on out it does two cycles that only have one difference to I'll state
+        when its the same by referencing the old step
+         */
+        telemetry.addData("Step 46", "Signaling Ready For Pass-off");//Telling the other bots it's done
         Arm.setTargetPosition(80);
         while(true){
             ClawPos(.4);
@@ -326,7 +330,10 @@ public class Ex extends Base {
             }
         }
         TelemeryClear();
-        telemetry.addData("Step 47","Driving Forward");
+
+
+
+        telemetry.addData("Step 47","Driving Forward");//finds the delivered supply
         telemetry.update();
         TelemeryClear();
         Sensor(36);
@@ -336,37 +343,37 @@ public class Ex extends Base {
         sleep(400);
         Arm.setTargetPosition(30);
 
-        telemetry.addData("Step 48","Driving Backwards");
+        telemetry.addData("Step 48","Driving Backwards");//Drives back into the auto zone
         telemetry.update();
         drive(-distanceTravelInches,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 49","Turning Left");
+        telemetry.addData("Step 49","Turning Left");//turns to stare inside the hallway
         telemetry.update();
         turnTo(90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 50","Driving Forward");
+        telemetry.addData("Step 50","Driving Forward");//Drives into the hallway
         telemetry.update();
         drive(17,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 51","Turning Left");
+        telemetry.addData("Step 51","Turning Left");//turns toward the safe zone side of the hallway
         telemetry.update();
         turnTo(180,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 52","Driving Forward");
+        telemetry.addData("Step 52","Driving Forward");//Drives to the hallway safe zone wall
         telemetry.update();
         drive(62,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 53","Turning Left");
+        telemetry.addData("Step 53","Turning Left");//Turns to stare at the safe zone
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 54","Driving Forwards");
+        telemetry.addData("Step 54","Driving Forwards");//Drives and drops off the supply
         telemetry.update();
         drive(10,0.3);
         TelemeryClear();
@@ -375,37 +382,37 @@ public class Ex extends Base {
         ClawPos(0.4);
         sleep(500);
 
-        telemetry.addData("Step 55","Driving Backwards");
+        telemetry.addData("Step 55","Driving Backwards");//backs out of safe zone
         telemetry.update();
         drive(-10,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 56","turning Left");
+        telemetry.addData("Step 56","turning Left");//Turns to stare at the entrance hallway wall
         telemetry.update();
         turnTo(0,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 57","Driving Forwards");
+        telemetry.addData("Step 57","Driving Forwards");//Drives to the end of the hallway entrance wall
         telemetry.update();
         drive(62,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 57","Turning Right");
+        telemetry.addData("Step 57","Turning Right");//turns to look at the auto zone entrance
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 59","Driving Forward");
+        telemetry.addData("Step 59","Driving Forward");//drives into the entrance zone
         telemetry.update();
         drive(17,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 60","Turning Left");
+        telemetry.addData("Step 60","Turning Left");//Turns to look outside the auto zone
         telemetry.update();
         turnTo(0,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 61", "Signaling Ready For Pass-off");
+        telemetry.addData("Step 61", "Signaling Ready For Pass-off");//Same as step 46
         Arm.setTargetPosition(80);
         while(true){
             ClawPos(.4);
@@ -422,44 +429,44 @@ public class Ex extends Base {
         }
         TelemeryClear();
 
-        telemetry.addData("Step 62", "Driving Forward");
+        telemetry.addData("Step 62", "Driving Forward");//Same as step 47
         telemetry.update();
         TelemeryClear();
         Sensor(36);
          distanceTravelTicks = LeftWheel.getCurrentPosition();
          distanceTravelInches = distanceTravelTicks / COUNTS_PER_INCH;
 
-        telemetry.addData("Step 63","Driving Backwards");
+        telemetry.addData("Step 63","Driving Backwards");//Same as step 48
         telemetry.update();
         drive(-distanceTravelInches,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 64","Turning Left");
+        telemetry.addData("Step 64","Turning Left");//Same as step 49
         telemetry.update();
         turnTo(90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 65","Driving Forward");
+        telemetry.addData("Step 65","Driving Forward");//Same as step 50
         telemetry.update();
         drive(17,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 66","Turning Left");
+        telemetry.addData("Step 66","Turning Left");//Same as step 51
         telemetry.update();
         turnTo(180,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 67","Driving Forward");
+        telemetry.addData("Step 67","Driving Forward");//Same as step 52
         telemetry.update();
         drive(62,0.3);
         TelemeryClear();
 
-        telemetry.addData("Step 68","Turning Left");
+        telemetry.addData("Step 68","Turning Left");//Same as step 53
         telemetry.update();
         turnTo(-90,0.2);
         TelemeryClear();
 
-        telemetry.addData("Step 69","Driving Forwards");
+        telemetry.addData("Step 69","Driving Forwards");//Same as step 54
         telemetry.update();
         drive(10,0.3);
         TelemeryClear();
@@ -468,7 +475,7 @@ public class Ex extends Base {
         ClawPos(0.4);
         sleep(500);
 
-        telemetry.addData("Step 70","Driving Backwards");
+        telemetry.addData("Step 70","Driving Backwards");//Same as step 55
         telemetry.update();
         drive(-10,0.3);
         TelemeryClear();
